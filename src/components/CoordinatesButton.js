90018ec269bc791f490a97633dfc48bb1b008af6
paulleerentveld <paul@veldnet.com.au>
2022-05-09 12:20:04 +1000

    lab changes

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,17 @@
 // Code CoordinatesButton Component Here
+import React, { Component } from "react";
+
+export default class CoordinatesButton extends Component {
+
+handleClick = (event) => {
+    //console.log(event.clientX,event.clientY)
+    let arr = [];
+    arr = [event.clientX,event.clientY];
+    //console.log(arr);
+    return this.props.onReceiveCoordinates(arr);
+}
+
+  render() {
+    return <button onClick={this.handleClick} >Coordinates Button</button>
+  }
+}
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,17 @@
 // Code DelayedButton Component Here
+import React, { Component } from "react";
+
+export default class DelayedButton extends Component {
+
+onDelayedClickHandler = (e) => {
+  //console.log(e)
+  e.persist();
+  return setTimeout(() => this.props.onDelayedClick(e),this.props.delay); 
+  //return setTimeout(() => this.props.onDelayedClick(e.target),this.props.delay) 
+}
+
+
+  render() {
+    return <button onClick={this.onDelayedClickHandler} >Delayed Button</button>
+  }
+}
